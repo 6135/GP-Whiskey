5472d3268382a8f382b39f5a76b4703ba25c55b9
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-11-23 21:21:43 +0000

    for testing purposes

---
+++ frontend/src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Homepage from '../components/homepage/homepage';
+
+test('renders learn react link', () => {
+  render(<Homepage />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
