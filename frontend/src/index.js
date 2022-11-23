5281b0e3534f02fc44e484977af72241efc7e0d7
diogocruz40 <diogo.e.cruz@hotmail.com>
2022-11-23 21:24:07 +0000

    fix reportwebvitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Homepage from './components/homepage/homepage';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './tests/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
