e9b30afe18844639a131868f1388f04d9ca2edf3
Cláudia Campos <45832743+TaurusLegend@users.noreply.github.com>
2022-12-02 19:25:43 +0000

    Adicionar Modelo da Obra

--- backend/gp_whiskey/constructions/models.py
+++ backend/gp_whiskey/constructions/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 #Basically the main models go here (e.g. obra, medicoes, gastos, etc)
+
+class Obra(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    #client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    nome = models.CharField()
+    data_inicio = models.DateTimeField()
+    data_conclusao = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
