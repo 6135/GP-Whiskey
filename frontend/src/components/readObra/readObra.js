f580f5b82933af7166b9ca26051e0be5e7bcd380
Diogo Cruz <diogo.e.cruz@hotmail.com>
2023-01-22 17:56:00 +0000

    react update in docker, and fix obras

--- frontend/src/components/readObra/readObra.js
+++ frontend/src/components/readObra/readObra.js
@@ -1,20 +1,54 @@
-import { GetAPI } from '../../hooks/serviceapi';
-import React from 'react';
+import { getAPI } from '../../hooks/serviceapi';
+import './readObra.css';
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
 
 function ReadObra() {
+  const [obra, setObra] = useState(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    getAPI("http://127.0.0.1:8000/constructions/obra").then(result => {
+      console.log(result);
+      if(result.status != "nao existem obras")
+        setObra(result);
+    })
 
-  //const obra = Request("http://127.0.0.1:8000/api/obra")
-  const obra = GetAPI("http://127.0.0.1:8000/api/obra");
-  console.log(obra);
+  }, []);
 
+  function handleDetails(event) {
+    let v = event.target.value;
+    navigate('/detailsobra', { state: { obraid: v, } });
+  }
   return (
-    <div className="ReadObra">
-      <div className='list-container'>
-        {Object.entries(obra)
-          .map(([key, value]) => (
-            <p>{value}</p>
-          ))}
+    <div>
+      <h1>
+        Listagem de Obras
+      </h1>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Nome</th>
+              <th>Data de Início</th>
+              <th>Data de Conclusão</th>
+            </tr>
+            {obra && obra.map(item => {
+
+              return (
+                <tr key={item.id}>
+                  <td>{item.nome}</td>
+                  <td>{item.data_inicio}</td>
+                  <td>{item.data_conclusao}</td>
+                  <td>
+                    <button className="button" value={item.id} onClick={handleDetails}>Details</button>
+                    <button className="button_delete" value={item.id}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
