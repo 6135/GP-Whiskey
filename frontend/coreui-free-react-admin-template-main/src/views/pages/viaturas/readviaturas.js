65de42485ec73e85a6b3ae075de5397f08db38c9
6135 <guibscosta@hotmail.com>
2023-01-29 01:05:13 +0000

    Viaturas list done ish

--- frontend/coreui-free-react-admin-template-main/src/views/pages/viaturas/readviaturas.js
+++ frontend/coreui-free-react-admin-template-main/src/views/pages/viaturas/readviaturas.js
@@ -1,108 +1,271 @@
 import React, { useState, useEffect } from 'react'
 import { getAPI } from '../../../hooks/serviceapi';
 import { useNavigate } from "react-router-dom";
-import { NavLink } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom';
 import {
-  CCardTitle,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CCardSubtitle,
-  CCardText,
-  CCardHeader,
-  CNavLink,
-  CNavItem,
-  CRow,
-  CFormInput,
-  CButton,
-  CHeaderNav,
-  CFormCheck,
+	CCardTitle,
+	CCard,
+	CCardBody,
+	CCardGroup,
+	CCol,
+	CContainer,
+	CForm,
+	CCardSubtitle,
+	CCardText,
+	CCardHeader,
+	CNavLink,
+	CNavItem,
+	CRow,
+	CFormInput,
+	CButton,
+	CHeaderNav,
+	CFormCheck,
+	CTable,
+	CTableHead,
+	CTableBody,
+	CTableRow,
+	CTableHeaderCell,
+	CTableDataCell,
+	CCollapse
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
-import { cilPencil, cilUserX,cilPlus,cilSearch} from '@coreui/icons';
-import { CCollapse } from '@coreui/react'
+import { cilLockLocked, cilUser, cilPencil, cilUserX, cilPlus, cilSearch } from '@coreui/icons'
+import DataTable from 'react-data-table-component';
 
 
-function ReadGastosObra  ()  {
-   
-  return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-top">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={9}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                    <CRow >
-                        <CHeaderNav >
-                            <h1>Viaturas</h1>
-                            <CForm className="d-flex" style={{ maxHeight: '75%' , marginLeft: '10Rem' }}>
-                                <CFormInput type="search" placeholder="Search" />
-                                <CButton type="submit" color="dark" variant="outline">
-                                    <CIcon icon={cilSearch} size="xl"/>
-                                </CButton>
-                            </CForm>
+const columns = [
+	{
+		name: 'Matricula',
+		selector: row => row.matricula,
+		sortable: true,
+		reorder: true,
+	},
+	{
+		name: 'Marca',
+		selector: row => row.marca,
+		sortable: true,
+		reorder: true,
+	},
+	{
+		name: 'Ano',
+		selector: row => row.ano,
+		sortable: true,
+		reorder: true,
+	},
+	{
+		name: 'Data Inicio',
+		selector: row => row.data_inicio,
+		sortable: true,
+		reorder: true,
+	},
+	{
+		name: 'Data fim',
+		selector: row => row.data_fim,
+		sortable: true,
+		reorder: true,
+	},
+];
 
-                            <CNavLink to="/addviaturas" component={NavLink} style={{ marginLeft: '1Rem' }}>
-                                <CCard className= {`mb-3 border-${"dark"}`}>
-                                        <CIcon icon={cilPlus} size="3xl"/>
-                                </CCard>
-                            </CNavLink>
-{/*                                 <CFormCheck id="flexCheckDefault" label="Arquivadas" style={{ marginLeft: '1Rem' }} />
- */}                        </CHeaderNav>
-                    </CRow>
+const temp = [{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},{
+	matricula: 		"Mat1",
+	marca: 			"BWM",
+	ano: 			"2030",
+	seguradora: 	"Fidelidade",
+	data_inicio: 	"01-01-2023",
+	data_fim: 		"01-01-2024",
+}, {
+	matricula: 		"Mat2",
+	marca: 			"Opel",
+	ano: 			"2029",
+	seguradora: 	"Seguros",
+	data_inicio: 	"01-01-2021",
+	data_fim: 		"02-02-2022",
+},];
+function ReadViaturas() {
+	const [viaturaSearch, setViaturaSearch] = useState(null);
+	const [pendingViaturas, setPendingViaturas] = React.useState(true);
+	const [viaturas, setViaturas] = React.useState([]);
+	const [filteredViaturas, setFilteredViaturas] = React.useState([]);
 
-                    <p className="text-medium-emphasis">Viaturas da Obra X</p>
-                    
-                                    <>
-                {[
-                    /* { color: 'primary', textColor: 'primary' },
-                    { color: 'secondary', textColor: 'secondary' },
-                    { color: 'success', textColor: 'success' },
-                    { color: 'danger', textColor: 'danger' },
-                    { color: 'warning', textColor: 'warning' },
-                    { color: 'info', textColor: 'info' }, 
-                    colocar array de funcionarios */
-                    { color: 'light' },
-                    { color: 'dark' },
-                ].map((item, index) => (
-                    <CCard
-                    // textColor={item.textColor}
-                    className={`mb-3 border-top-${'dark'} border-top-3`}
-                    style={{ maxWidth: '100%' }}
-                    key={index}
-                    >
-                    <CCardHeader>
-                        <CHeaderNav>
-                        <h5>Nome da Viatura A</h5>
-                            <CNavLink to="/dashboard" component={NavLink}>
-                                <CIcon icon={cilPencil} size="xl"/>
-                                {/* editar utilizador somehow passar dados do user p la 
-                                    usar item.? */}
-                            </CNavLink>
-                        </CHeaderNav>
-                    </CCardHeader>
-                    <CCardBody>
-                        {/* <CCardTitle>{item.color} card title</CCardTitle> */}
-                        <h6>Marca </h6>
-                        <h6>Matrícula </h6>
-                    </CCardBody>
-                    </CCard>
-                ))}
-                </>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
-  )
+	// fakeUsers.filter(
+	// 	item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()),
+	// );
+
+	function handleSearchViatura(event) {
+		setViaturaSearch(event.target.value);
+		console.log(event.target.value);
+		setFilteredViaturas(
+			viaturas.filter(viatura => {
+				return (viatura.matricula && viatura.matricula.toLowerCase().includes(event.target.value.toLowerCase())) ||
+					(viatura.marca && viatura.marca.toLowerCase().includes(event.target.value.toLowerCase())) ||
+					(viatura.ano && viatura.ano.toLowerCase().includes(event.target.value.toLowerCase())) ||
+					(viatura.seguradora && viatura.seguradora.toLowerCase().includes(event.target.value.toLowerCase())) ||
+					(viatura.data_inicio && viatura.data_inicio.toLowerCase().includes(event.target.value.toLowerCase())) ||
+					(viatura.data_fim && viatura.data_fim.toLowerCase().includes(event.target.value.toLowerCase()))
+
+
+
+			}));
+	}
+
+	useEffect(() => {
+
+		//getAPI to get viaturas of an obra
+
+		
+		const timeout = setTimeout(() => {
+			setViaturas(temp);
+			setFilteredViaturas(temp);
+			setPendingViaturas(false);
+		}, 2000);
+		return () => clearTimeout(timeout);
+
+	}, [])
+
+
+	return (
+		<div className="bg-light min-vh-100 d-flex flex-row align-items-top">
+			<CContainer>
+				<CRow className="justify-content-center">
+					<CCol md={9}>
+						<CCardGroup>
+							<CCard className="p-4">
+								<CCardBody>
+									<CRow >
+										<CHeaderNav >
+											<h1>Viaturas</h1>
+											<CForm className="d-flex" style={{ maxHeight: '75%', marginLeft: '10Rem' }}>
+												<CFormInput type="search" placeholder="Search" onChange={handleSearchViatura} />
+												<CButton type="submit" color="dark" variant="outline">
+													<CIcon icon={cilSearch} size="xl" />
+												</CButton>
+											</CForm>
+
+											<CNavLink to="/addviaturas" component={NavLink} style={{ marginLeft: '1Rem' }}>
+												<CCard className={`mb-3 border-${"dark"}`}>
+													<CIcon icon={cilPlus} size="3xl" />
+												</CCard>
+											</CNavLink>
+										</CHeaderNav>
+									</CRow>
+									
+									<DataTable
+									
+										striped
+										pagination
+										columns={columns}
+										data={filteredViaturas}
+										progressPending={pendingViaturas}
+										highlightOnHover
+										paginationPerPage={5}
+										paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 50, 75, 100]}
+									/>
+								</CCardBody>
+							</CCard>
+						</CCardGroup>
+					</CCol>
+				</CRow>
+			</CContainer>
+		</div>
+	)
 }
 
-export default ReadGastosObra
+export default ReadViaturas
